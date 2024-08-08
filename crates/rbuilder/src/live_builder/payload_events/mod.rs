5c0d9cd28ee41a977bd849256e55bc70ce736aae
Daniel Xifra <mandinga@gmail.com>
2024-08-08 10:49:03 -0300

    removed "as hack"

--- crates/rbuilder/src/live_builder/payload_events/mod.rs
+++ crates/rbuilder/src/live_builder/payload_events/mod.rs
@@ -102,7 +102,7 @@ impl MevBoostSlotDataGenerator {
     /// Spawns the reader task.
     /// It reads from a PayloadSourceMuxer, replaces the fee_recipient/gas_limit with the info from the relays and filters duplicates.
     /// Why it replaces the fee_recipient?
-    ///     MEV-boost was built as a hack on top of eth 2.0.
+    ///     MEV-boost was built on top of eth 2.0.
     ///     Usually (non MEV-boost) the CL only cares about notifying slots to the EL for the slots it should build (once every 2 months!).
     ///     When MEV-boost is used we tell the CL “--always-build-payload” (we are building blocks for ANY validator now!) and the CL does
     ///     it but, even with the event being created for every slot, the fee_recipient we get from MEV-Boost might be different so should always be used instead.
