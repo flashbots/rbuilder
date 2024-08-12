8451f3030a92999ca5dec341ffbc4e963bf55f7a
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-08-12 19:14:23 +0100

    Move relay config to relay crate (#111)
    ## 📝 Summary
    
    This PR moves the `RelayConfig` to the relay crate. It includes a unit
    test to ensure the backward compatibility for reading env variables in
    the config toml file.
    
    ## 💡 Motivation and Context
    
    Move L1 specific configuration out of the Live builder to their own
    repositories.
    
    ---
    
    ## ✅ I have completed the following steps:
    
    * [x] Run `make lint`
    * [x] Run `make test`
    * [x] Added tests (if applicable)

--- crates/rbuilder/src/bin/dummy-builder.rs
+++ crates/rbuilder/src/bin/dummy-builder.rs
@@ -31,7 +31,10 @@ use rbuilder::{
         simulation::SimulatedOrderCommand,
         LiveBuilder,
     },
-    primitives::{mev_boost::MevBoostRelay, SimulatedOrder},
+    primitives::{
+        mev_boost::{MevBoostRelay, RelayConfig},
+        SimulatedOrder,
+    },
     roothash::RootHashMode,
     utils::Signer,
 };
@@ -55,18 +58,12 @@ async fn main() -> eyre::Result<()> {
     let chain_spec = MAINNET.clone();
     let cancel = CancellationToken::new();
     let bidding_service = Box::new(DummyBiddingService {});
-    let relay = MevBoostRelay::try_from_name_or_url(
-        "flashbots",
-        "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net",
-        0,
-        false,
-        false,
-        false,
-        None,
-        None,
-        None,
-        None,
-    )?;
+
+    let relay_config = RelayConfig::default().
+        with_url("https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net").
+        with_name("flashbots");
+
+    let relay = MevBoostRelay::from_config(&relay_config)?;
 
     let builder = LiveBuilder::<Arc<DatabaseEnv>, TraceBlockSinkFactory> {
         cls: vec![Client::default()],
