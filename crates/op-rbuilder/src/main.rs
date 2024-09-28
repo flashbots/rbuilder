576c8a9f0a46c1f02996155b62aa38d310c4c022
Liam Aharon <liam.aharon@hotmail.com>
2024-09-28 15:19:06 +0100

    add a custom payload builder

--- crates/op-rbuilder/src/main.rs
+++ crates/op-rbuilder/src/main.rs
@@ -5,18 +5,20 @@
 //! It is planned to handle `eth_sendBundle` requests, revert protection, and
 //! some other features.
 
+#![cfg_attr(all(not(test), feature = "optimism"), warn(unused_crate_dependencies))]
+// The `optimism` feature must be enabled to use this crate.
+#![cfg(feature = "optimism")]
+
 mod eth_bundle_api;
 mod node;
+
+use tracing as _;
 
 // jemalloc provides better performance
 #[cfg(all(feature = "jemalloc", unix))]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-#[cfg(not(feature = "optimism"))]
-compile_error!("Cannot build the `op-rbuilder` binary without the `optimism` feature flag enabled. Did you mean to build `rbuilder`?");
-
-#[cfg(feature = "optimism")]
 fn main() {
     reth_cli_util::sigsegv_handler::install();
 
--- crates/op-rbuilder/src/node.rs
+++ crates/op-rbuilder/src/node.rs
@@ -1,16 +1,22 @@
 use clap::Parser;
 use reth::cli::Cli;
-use reth_node_optimism::{args::RollupArgs, OptimismNode};
+use reth_node_optimism::{args::RollupArgs, node::OptimismAddOns, OptimismEvmConfig, OptimismNode};
 use reth_optimism_rpc::eth::rpc::SequencerClient;
 use reth_rpc_api::EthBundleApiServer;
 
 use crate::eth_bundle_api;
+use op_rbuilder_payload_builder::CustomPayloadBuilder;
 
 pub fn run() {
     if let Err(err) = Cli::<RollupArgs>::parse().run(|builder, rollup_args| async move {
         let sequencer_http_arg = rollup_args.sequencer_http.clone();
         let handle = builder
-            .node(OptimismNode::new(rollup_args.clone()))
+            .with_types::<OptimismNode>()
+            .with_components(
+                OptimismNode::components(rollup_args)
+                    .payload(CustomPayloadBuilder::new(OptimismEvmConfig::default())),
+            )
+            .with_add_ons::<OptimismAddOns>()
             .extend_rpc_modules(move |ctx| {
                 // register sequencer tx forwarder
                 if let Some(sequencer_http) = sequencer_http_arg {
