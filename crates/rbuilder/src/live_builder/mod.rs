1ce7843b929e77804b7fb47f4d63dc3d26b98ea6
Ferran Borreguero <ferran.borreguero@gmail.com>
2024-12-31 18:37:44 +0100

    Notify orderpool cleanup without IPC

--- crates/rbuilder/src/live_builder/mod.rs
+++ crates/rbuilder/src/live_builder/mod.rs
@@ -151,6 +151,8 @@ where
         let mut inner_jobs_handles = Vec::new();
         let mut payload_events_channel = self.blocks_source.recv_slot_channel();
 
+        let (header_sender, header_receiver) = mpsc::channel(10);
+
         let orderpool_subscriber = {
             let (handle, sub) = start_orderpool_jobs(
                 self.order_input_config,
@@ -159,6 +161,7 @@ where
                 self.global_cancellation.clone(),
                 self.orderpool_sender,
                 self.orderpool_receiver,
+                header_receiver,
             )
             .await?;
             inner_jobs_handles.push(handle);
@@ -247,6 +250,11 @@ where
                 parent_hash = ?payload.parent_block_hash(),
                 "Got header for slot"
             );
+
+            // notify the order pool that there is a new header
+            if let Err(err) = header_sender.send(parent_header.clone()).await {
+                warn!("Failed to send header to builder pool: {:?}", err);
+            }
 
             inc_active_slots();
 
