a63f1e0d7daca13e8b86105d7d5b7860e730282a
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-01-07 17:26:10 +0000

    Alternative

---
+++ crates/op-rbuilder-vanilla/src/generator.rs
@@ -0,0 +1,99 @@
+use crate::job::EmptyBlockPayloadJob;
+use alloy_eips::BlockNumberOrTag;
+use alloy_primitives::Bytes;
+use reth::{
+    providers::{BlockReaderIdExt, BlockSource, StateProviderFactory},
+    tasks::TaskSpawner,
+    transaction_pool::TransactionPool,
+};
+use reth_basic_payload_builder::{BasicPayloadJobGeneratorConfig, PayloadBuilder, PayloadConfig};
+use reth_node_api::PayloadBuilderAttributes;
+use reth_payload_builder::{PayloadBuilderError, PayloadJobGenerator};
+use reth_primitives::SealedHeader;
+use std::sync::Arc;
+
+/// The generator type that creates new jobs that builds empty blocks.
+#[derive(Debug)]
+pub struct EmptyBlockPayloadJobGenerator<Client, Pool, Tasks, Builder> {
+    /// The client that can interact with the chain.
+    client: Client,
+    /// txpool
+    pool: Pool,
+    /// How to spawn building tasks
+    executor: Tasks,
+    /// The configuration for the job generator.
+    _config: BasicPayloadJobGeneratorConfig,
+    /// The type responsible for building payloads.
+    ///
+    /// See [PayloadBuilder]
+    builder: Builder,
+}
+
+// === impl EmptyBlockPayloadJobGenerator ===
+
+impl<Client, Pool, Tasks, Builder> EmptyBlockPayloadJobGenerator<Client, Pool, Tasks, Builder> {
+    /// Creates a new [EmptyBlockPayloadJobGenerator] with the given config and custom
+    /// [PayloadBuilder]
+    pub fn with_builder(
+        client: Client,
+        pool: Pool,
+        executor: Tasks,
+        config: BasicPayloadJobGeneratorConfig,
+        builder: Builder,
+    ) -> Self {
+        Self {
+            client,
+            pool,
+            executor,
+            _config: config,
+            builder,
+        }
+    }
+}
+
+impl<Client, Pool, Tasks, Builder> PayloadJobGenerator
+    for EmptyBlockPayloadJobGenerator<Client, Pool, Tasks, Builder>
+where
+    Client: StateProviderFactory + BlockReaderIdExt + Clone + Unpin + 'static,
+    Pool: TransactionPool + Unpin + 'static,
+    Tasks: TaskSpawner + Clone + Unpin + 'static,
+    Builder: PayloadBuilder<Pool, Client> + Unpin + 'static,
+    <Builder as PayloadBuilder<Pool, Client>>::Attributes: Unpin + Clone,
+    <Builder as PayloadBuilder<Pool, Client>>::BuiltPayload: Unpin + Clone,
+{
+    type Job = EmptyBlockPayloadJob<Client, Pool, Tasks, Builder>;
+
+    /// This is invoked when the node receives payload attributes from the beacon node via
+    /// `engine_forkchoiceUpdatedV1`
+    fn new_payload_job(
+        &self,
+        attributes: <Builder as PayloadBuilder<Pool, Client>>::Attributes,
+    ) -> Result<Self::Job, PayloadBuilderError> {
+        let parent_block = if attributes.parent().is_zero() {
+            // use latest block if parent is zero: genesis block
+            self.client
+                .block_by_number_or_tag(BlockNumberOrTag::Latest)?
+                .ok_or_else(|| PayloadBuilderError::MissingParentBlock(attributes.parent()))?
+                .seal_slow()
+        } else {
+            let block = self
+                .client
+                .find_block_by_hash(attributes.parent(), BlockSource::Any)?
+                .ok_or_else(|| PayloadBuilderError::MissingParentBlock(attributes.parent()))?;
+
+            // we already know the hash, so we can seal it
+            block.seal(attributes.parent())
+        };
+        let hash = parent_block.hash();
+        let header = SealedHeader::new(parent_block.header().clone(), hash);
+
+        let config = PayloadConfig::new(Arc::new(header), Bytes::default(), attributes);
+        Ok(EmptyBlockPayloadJob {
+            client: self.client.clone(),
+            _pool: self.pool.clone(),
+            _executor: self.executor.clone(),
+            builder: self.builder.clone(),
+            config,
+        })
+    }
+}
---
+++ crates/op-rbuilder-vanilla/src/main.rs
@@ -0,0 +1,106 @@
+use clap::Parser;
+use generator::EmptyBlockPayloadJobGenerator;
+use reth::{
+    builder::{components::PayloadServiceBuilder, node::FullNodeTypes, BuilderContext},
+    payload::PayloadBuilderHandle,
+    providers::CanonStateSubscriptions,
+    transaction_pool::TransactionPool,
+};
+use reth::{
+    builder::{engine_tree_config::TreeConfig, EngineNodeLauncher},
+    providers::providers::BlockchainProvider2,
+};
+use reth_basic_payload_builder::BasicPayloadJobGeneratorConfig;
+use reth_node_api::NodeTypesWithEngine;
+use reth_optimism_chainspec::OpChainSpec;
+use reth_optimism_cli::{chainspec::OpChainSpecParser, Cli};
+use reth_optimism_evm::OpEvmConfig;
+use reth_optimism_node::OpEngineTypes;
+use reth_optimism_node::{args::RollupArgs, node::OptimismAddOns, OptimismNode};
+use reth_payload_builder::PayloadBuilderService;
+
+pub mod generator;
+pub mod job;
+
+#[derive(Debug, Clone, Copy, Default)]
+#[non_exhaustive]
+pub struct CustomPayloadBuilder;
+
+impl<Node, Pool> PayloadServiceBuilder<Node, Pool> for CustomPayloadBuilder
+where
+    Node:
+        FullNodeTypes<Types: NodeTypesWithEngine<Engine = OpEngineTypes, ChainSpec = OpChainSpec>>,
+    Pool: TransactionPool + Unpin + 'static,
+{
+    async fn spawn_payload_service(
+        self,
+        ctx: &BuilderContext<Node>,
+        pool: Pool,
+    ) -> eyre::Result<PayloadBuilderHandle<<Node::Types as NodeTypesWithEngine>::Engine>> {
+        tracing::info!("Spawning a custom payload builder");
+        let _conf = ctx.payload_builder_config();
+
+        let payload_job_config = BasicPayloadJobGeneratorConfig::default();
+
+        let payload_generator = EmptyBlockPayloadJobGenerator::with_builder(
+            ctx.provider().clone(),
+            pool,
+            ctx.task_executor().clone(),
+            payload_job_config,
+            reth_optimism_payload_builder::OpPayloadBuilder::new(OpEvmConfig::new(
+                ctx.chain_spec(),
+            )),
+        );
+
+        let (payload_service, payload_builder) =
+            PayloadBuilderService::new(payload_generator, ctx.provider().canonical_state_stream());
+
+        ctx.task_executor()
+            .spawn_critical("custom payload builder service", Box::pin(payload_service));
+
+        Ok(payload_builder)
+    }
+}
+
+fn main() {
+    Cli::<OpChainSpecParser, RollupArgs>::parse()
+        .run(|builder, rollup_args| async move {
+            if rollup_args.experimental {
+                tracing::warn!(target: "reth::cli", "Experimental engine is default now, and the --engine.experimental flag is deprecated. To enable the legacy functionality, use --engine.legacy.");
+            }
+            let use_legacy_engine = rollup_args.legacy;
+            let sequencer_http_arg = rollup_args.sequencer_http.clone();
+
+            match use_legacy_engine {
+                false => {
+                    let engine_tree_config = TreeConfig::default()
+                        .with_persistence_threshold(rollup_args.persistence_threshold)
+                        .with_memory_block_buffer_target(rollup_args.memory_block_buffer_target);
+                    let handle = builder
+                        .with_types_and_provider::<OptimismNode, BlockchainProvider2<_>>()
+                        .with_components(
+                            OptimismNode::components(rollup_args).payload(CustomPayloadBuilder::default()),
+                        )
+                        .with_add_ons(OptimismAddOns::new(sequencer_http_arg))
+                        .launch_with_fn(|builder| {
+                            let launcher = EngineNodeLauncher::new(
+                                builder.task_executor().clone(),
+                                builder.config().datadir(),
+                                engine_tree_config,
+                            );
+                            builder.launch_with(launcher)
+                        })
+                        .await?;
+
+                    handle.node_exit_future.await
+                },
+                true => {
+                    let handle =
+                        builder.node(OptimismNode::new(rollup_args.clone())).launch().await?;
+
+                    handle.node_exit_future.await
+                },
+            }
+        })
+        .unwrap();
+}
