2c77a50756e7b3afa45affb249d55d14e8330a0d
Daniel Xifra <mandinga@gmail.com>
2025-01-28 17:30:28 -0300

    tests

--- crates/rbuilder/src/live_builder/building/unfinished_block_building_sink_muxer.rs
+++ crates/rbuilder/src/live_builder/building/unfinished_block_building_sink_muxer.rs
@@ -96,3 +96,148 @@ impl BestBlockState {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use revm_primitives::U256;
+
+    use crate::{
+        building::{
+            builders::{
+                block_building_helper::BiddableUnfinishedBlock,
+                mock_block_building_helper::MockBlockBuildingHelper,
+            },
+            ExecutionResult,
+        },
+        primitives::{AccountNonce, Order},
+    };
+
+    use super::BestBlockState;
+
+    struct TestDataGenerator {
+        base: crate::primitives::TestDataGenerator,
+    }
+    impl TestDataGenerator {
+        fn new() -> Self {
+            Self {
+                base: Default::default(),
+            }
+        }
+
+        /// Creates a unique block with true_block_value / builder_name
+        fn create_block(
+            &mut self,
+            true_block_value: U256,
+            builder_name: &str,
+        ) -> BiddableUnfinishedBlock {
+            let mut block = MockBlockBuildingHelper::new(true_block_value, true)
+                .with_builder_name(builder_name.to_string());
+            let order = self.base.create_mempool_tx(AccountNonce::default());
+            let tx = order.tx_with_blobs.clone();
+            // Give unique identity
+            block
+                .built_block_trace_mut_ref()
+                .included_orders
+                .push(ExecutionResult {
+                    coinbase_profit: Default::default(),
+                    inplace_sim: Default::default(),
+                    gas_used: Default::default(),
+                    order: Order::Tx(order),
+                    txs: vec![tx],
+                    original_order_ids: Default::default(),
+                    receipts: Default::default(),
+                    nonces_updated: Default::default(),
+                    paid_kickbacks: Default::default(),
+                });
+            BiddableUnfinishedBlock::new(Box::new(block)).unwrap()
+        }
+    }
+
+    const NAME_1: &str = "NAME1";
+    const NAME_2: &str = "NAME2";
+    const LOW_VAL: u64 = 10;
+    const HIGH_VAL: u64 = 100;
+
+    #[test]
+    fn first_block() {
+        let mut data_gen = TestDataGenerator::new();
+        let mut state = BestBlockState::new();
+        let block = data_gen.create_block(U256::from(LOW_VAL), NAME_1);
+        let best_block = state.update_best_block(block.clone());
+        assert_eq!(
+            block.true_block_value(),
+            best_block.unwrap().true_block_value()
+        );
+    }
+
+    ///Send block and then send a better one from other builder, the new one should win.
+    #[test]
+    fn better_block_not_same_as_winning_builder() {
+        let mut data_gen = TestDataGenerator::new();
+        let mut state = BestBlockState::new();
+        let block_low = data_gen.create_block(U256::from(LOW_VAL), NAME_1);
+        let block_high = data_gen.create_block(U256::from(HIGH_VAL), NAME_2);
+        let _ = state.update_best_block(block_low);
+        let best_block = state.update_best_block(block_high.clone());
+        assert_eq!(
+            block_high.true_block_value(),
+            best_block.unwrap().true_block_value()
+        );
+    }
+
+    ///Send block and then send a better one from the same builder, the new one should win.
+    #[test]
+    fn better_block_same_winning_builder() {
+        let mut data_gen = TestDataGenerator::new();
+        let mut state = BestBlockState::new();
+        let block_low = data_gen.create_block(U256::from(LOW_VAL), NAME_1);
+        let block_high = data_gen.create_block(U256::from(HIGH_VAL), NAME_1);
+        let _ = state.update_best_block(block_low);
+        let best_block = state.update_best_block(block_high.clone());
+        assert_eq!(
+            block_high.true_block_value(),
+            best_block.unwrap().true_block_value()
+        );
+    }
+
+    /// Send block and then send a worse one from the same builder, should lower the bid
+    #[test]
+    fn worse_block_same_winning_builder() {
+        let mut data_gen = TestDataGenerator::new();
+        let mut state = BestBlockState::new();
+        let block_high = data_gen.create_block(U256::from(HIGH_VAL), NAME_1);
+        let block_low = data_gen.create_block(U256::from(LOW_VAL), NAME_1);
+
+        let _ = state.update_best_block(block_high);
+        let best_block = state.update_best_block(block_low.clone());
+        assert_eq!(
+            block_low.true_block_value(),
+            best_block.unwrap().true_block_value()
+        );
+    }
+
+    /// Send block and then send a worse one from other builder, should not bid
+    #[test]
+    fn worse_block_not_same_as_winning_builder() {
+        let mut data_gen = TestDataGenerator::new();
+        let mut state = BestBlockState::new();
+        let block_high = data_gen.create_block(U256::from(HIGH_VAL), NAME_1);
+        let block_low = data_gen.create_block(U256::from(LOW_VAL), NAME_2);
+
+        let _ = state.update_best_block(block_high);
+        let best_block = state.update_best_block(block_low.clone());
+        assert!(best_block.is_none());
+    }
+
+    /// Send the same winning block twice, second should be ignored
+    #[test]
+    fn exact_same_block_winning_builder() {
+        let mut data_gen = TestDataGenerator::new();
+        let mut state = BestBlockState::new();
+        let block = data_gen.create_block(U256::from(LOW_VAL), NAME_1);
+
+        let _ = state.update_best_block(block.clone());
+        let best_block = state.update_best_block(block);
+        assert!(best_block.is_none());
+    }
+}
