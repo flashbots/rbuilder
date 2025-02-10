8d64dee6fdff26e7d2a28b86b8191db29458c605
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-02-10 11:18:27 +0000

    Validate rbuilder with op-reth

--- crates/op-rbuilder/src/integration/integration_test.rs
+++ crates/op-rbuilder/src/integration/integration_test.rs
@@ -1,6 +1,8 @@
 #[cfg(all(test, feature = "integration"))]
 mod tests {
-    use crate::integration::{op_rbuilder::OpRbuilderConfig, IntegrationFramework};
+    use crate::integration::{
+        op_rbuilder::OpRbuilderConfig, op_reth::OpRethConfig, IntegrationFramework,
+    };
     use crate::tester::{BlockGenerator, EngineApi};
     use std::path::PathBuf;
     use uuid::Uuid;
@@ -16,20 +18,30 @@ mod tests {
         genesis_path.push("../../genesis.json");
         assert!(genesis_path.exists());
 
-        // generate a random dir for the data dir
-        let data_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
-
-        // generate a rra
-        let reth = OpRbuilderConfig::new()
-            .chain_config_path(genesis_path)
-            .data_dir(data_dir)
+        // create the builder
+        let builder_data_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
+        let builder = OpRbuilderConfig::new()
+            .chain_config_path(genesis_path.clone())
+            .data_dir(builder_data_dir)
             .auth_rpc_port(1234)
             .network_port(1235);
 
-        framework.start("op-rbuilder", &reth).await.unwrap();
+        framework.start("op-rbuilder", &builder).await.unwrap();
+
+        // create the validation reth node
+        let reth_data_dir = std::env::temp_dir().join(Uuid::new_v4().to_string());
+        let reth = OpRethConfig::new()
+            .chain_config_path(genesis_path)
+            .data_dir(reth_data_dir)
+            .auth_rpc_port(1236)
+            .network_port(1237);
+
+        framework.start("op-reth", &reth).await.unwrap();
 
         let engine_api = EngineApi::new("http://localhost:1234").unwrap();
-        let mut generator = BlockGenerator::new(&engine_api, None, false);
+        let validation_api = EngineApi::new("http://localhost:1236").unwrap();
+
+        let mut generator = BlockGenerator::new(&engine_api, Some(&validation_api), false);
         generator.init().await.unwrap();
 
         for _ in 0..10 {
