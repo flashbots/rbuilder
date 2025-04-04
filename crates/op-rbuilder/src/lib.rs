243741bb3a97016c91a0a9c1259bc91ba88ca161
Solar Mithril <solarmithril@pm.me>
2025-04-04 13:43:26 +0700

    Fix rollup-boost

--- crates/op-rbuilder/src/lib.rs
+++ crates/op-rbuilder/src/lib.rs
@@ -1,5 +1,4 @@
 pub mod integration;
 pub mod primitives;
-//TODO: disabled because of this https://github.com/flashbots/rollup-boost/pull/98, will enable once merged
-// pub mod tester;
+pub mod tester;
 pub mod tx_signer;
