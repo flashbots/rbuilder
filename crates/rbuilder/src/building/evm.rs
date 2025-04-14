320613f09ea57ac62e4cc17b0b5e6aea67084675
julio4 <30329843+julio4@users.noreply.github.com>
2025-04-14 10:53:45 +0800

    Revert "fix: use reth EvmFactory instead of Context"
    This reverts commit 1052c799c2483a41462344424919a0527ba086e5.

--- crates/rbuilder/src/building/evm.rs
+++ crates/rbuilder/src/building/evm.rs
@@ -1,8 +1,6 @@
 use crate::building::precompile_cache::{PrecompileCache, WrappedPrecompile};
 use parking_lot::Mutex;
-use reth_evm::{
-    eth::EthEvmContext, EthEvm, EthEvmFactory, Evm, EvmEnv, EvmFactory as RethEvmFactory,
-};
+use reth_evm::{eth::EthEvmContext, EthEvm, Evm, EvmEnv};
 use revm::{
     context::{
         result::{EVMError, HaltReason},
@@ -12,7 +10,7 @@ use revm::{
     inspector::NoOpInspector,
     interpreter::interpreter::EthInterpreter,
     primitives::hardfork::SpecId,
-    Database, Inspector,
+    Context, Database, Inspector, MainBuilder, MainContext,
 };
 use std::sync::Arc;
 
@@ -59,14 +57,18 @@ impl EvmFactory for EthCachedEvmFactory {
     where
         DB: Database<Error: Send + Sync + 'static>,
     {
-        let evm = EthEvmFactory::default()
-            .create_evm(db, env)
-            .into_inner()
-            .with_precompiles(WrappedPrecompile::new(
-                EthPrecompiles::default(),
-                self.cache.clone(),
-            ));
-        EthEvm::new(evm, false)
+        EthEvm::new(
+            Context::mainnet()
+                .with_block(env.block_env)
+                .with_cfg(env.cfg_env)
+                .with_db(db)
+                .build_mainnet_with_inspector(NoOpInspector {})
+                .with_precompiles(WrappedPrecompile::new(
+                    EthPrecompiles::default(),
+                    self.cache.clone(),
+                )),
+            false,
+        )
     }
 
     fn create_evm_with_inspector<DB, I>(
