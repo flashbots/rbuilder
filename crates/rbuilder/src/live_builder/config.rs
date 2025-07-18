09066e787d66fb7c723e0f5fb1e9de1e18b367a7
Alex Stokes <r.alex.stokes@gmail.com>
2025-07-18 12:23:55 -0600

    update `ethereum-consensus` and support more possible inputs for relay key config

--- crates/rbuilder/src/live_builder/config.rs
+++ crates/rbuilder/src/live_builder/config.rs
@@ -46,6 +46,7 @@ use crate::{
 };
 use alloy_chains::ChainKind;
 use alloy_primitives::{
+    hex,
     utils::{format_ether, parse_ether},
     FixedBytes, B256, U256,
 };
@@ -63,8 +64,8 @@ use reth_primitives::StaticFileSegment;
 use reth_provider::StaticFileProviderFactory;
 use serde::Deserialize;
 use serde_with::{serde_as, OneOrMany};
-use std::collections::HashMap;
 use std::{
+    collections::HashMap,
     fmt::Debug,
     path::{Path, PathBuf},
     str::FromStr,
@@ -286,7 +287,8 @@ impl L1Config {
 
         let relay_secret_key = if let Some(secret_key) = &self.relay_secret_key {
             let resolved_key = secret_key.value()?;
-            SecretKey::try_from(resolved_key)?
+            let input = hex::decode(resolved_key)?;
+            SecretKey::from_bytes(&input)?
         } else {
             warn!("No relay secret key provided. A random key will be generated.");
             SecretKey::random(&mut rand::thread_rng())?
