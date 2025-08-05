ba4379315b13aa1412c36f19f8a702050948e903
File Large <red.zone7786@fastmail.com>
2025-08-05 12:22:34 -0700

    fix

--- crates/rbuilder/src/mev_boost/mod.rs
+++ crates/rbuilder/src/mev_boost/mod.rs
@@ -874,7 +874,7 @@ mod tests {
         let mut generator = TestDataGenerator::default();
 
         let relay_url = Url::from_str(&srv.endpoint()).unwrap();
-        let relay = RelayClient::from_url(relay_url, None, None, None, false, false);
+        let relay = RelayClient::from_url(relay_url, None, None, None, false, false, false);
         let submission = SubmitBlockRequest::Deneb(generator.create_deneb_submit_block_request());
         let sub_relay = SubmitBlockRequestWithMetadata {
             submission,
