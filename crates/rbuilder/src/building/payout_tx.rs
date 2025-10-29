83d8bcf71f881f18aadb4886cd1e18280a338167
File Large <red.zone7786@fastmail.com>
2025-10-29 09:40:33 +0100

    fix: payout estimation gas limit

--- crates/rbuilder/src/building/payout_tx.rs
+++ crates/rbuilder/src/building/payout_tx.rs
@@ -133,7 +133,7 @@ fn estimate_payout_tx_space(ctx: &BlockBuildingContext) -> Result<BlockSpace, se
         &ctx.builder_signer,
         0,
         Address::ZERO,
-        ctx.evm_env.block_env.gas_limit,
+        ctx.evm_env.cfg_env.tx_gas_limit_cap,
         U256::ZERO,
     )?;
     Ok(BlockSpace::new(
@@ -159,10 +159,12 @@ pub fn estimate_payout_gas_limit(
     }
 
     // We probably have a bug here since no reserved space was propagated but with a little bit of luck it will work because we call can_fit_tx later?
-    let gas_left = ctx
+    let max_tx_gas_limit = ctx
         .evm_env
-        .block_env
-        .gas_limit
+        .cfg_env
+        .tx_gas_limit_cap
+        .unwrap_or(ctx.evm_env.block_env.gas_limit);
+    let gas_left = max_tx_gas_limit
         .checked_sub(space_used.gas)
         .unwrap_or_default();
     let estimation = insert_test_payout_tx(to, ctx, local_ctx, state, gas_left)?
