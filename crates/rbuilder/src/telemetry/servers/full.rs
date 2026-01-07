06782349e6990fefe365bfa967058a4c78ef1bc1
Daniel Xifra <daniel@xifra.dev>
2026-01-07 16:35:32 -0300

    forgot it

--- crates/rbuilder/src/telemetry/servers/full.rs
+++ crates/rbuilder/src/telemetry/servers/full.rs
@@ -12,13 +12,11 @@ use std::net::SocketAddr;
 use time::OffsetDateTime;
 use warp::{Filter, Rejection, Reply};
 
-use crate::{
-    telemetry::{
-        metrics::{gather_prometheus_metrics, set_version},
-        tokio::register_tokio_metrics,
-        BUILDER_BALANCE, CURRENT_BLOCK, MAX_FRESH_GAUGE_AGE, ORDERPOOL_BUNDLES, ORDERPOOL_TXS,
-        ORDERPOOL_TXS_SIZE, REGISTRY,
-    },
+use crate::telemetry::{
+    metrics::{gather_prometheus_metrics, set_version},
+    tokio::register_tokio_metrics,
+    BUILDER_BALANCE, CURRENT_BLOCK, MAX_FRESH_GAUGE_AGE, ORDERPOOL_BUNDLES, ORDERPOOL_TXS,
+    ORDERPOOL_TXS_SIZE, REGISTRY,
 };
 
 pub async fn spawn(addr: SocketAddr, version: Version) -> eyre::Result<()> {
